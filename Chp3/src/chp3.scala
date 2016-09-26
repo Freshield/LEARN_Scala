e7daff83ecc02063b39a5db4c8dc8e1c3fb01df4
Freshield <zxdsw199182@gmail.com>
2016-09-25 20:54:36 -0400

    interrupt

--- Chp3/src/chp3.scala
+++ Chp3/src/chp3.scala
@@ -1,10 +1,29 @@
+import java.math.BigInteger
+
 /**
-  * Created by FRESHIELD on 2016/9/26.
+  * Created by FRESHIELD on 2016/9/25.
   */
-class chp3 {
+object chp3 {
 
   def main(args: Array[String]) {
-    
+    println("here")
+
+    val big = new BigInteger("12345")
+    println(big)
+
+    val greetString = new Array[String](3)
+
+    greetString(0) = "hello"
+    greetString(1) = ","
+    greetString(2) = "world!\n"
+
+    for (i <- 0.to(2))
+      print(greetString.apply(i))
+
+    println(1.+(2))
+
+    val numNames = Array("zero","one","two")
+    numNames.foreach(println)
   }
 
 }
