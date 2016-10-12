92103ea92816ed592cc2bbc03c71340d4e911507
Freshield <zxdsw199182@gmail.com>
2016-10-12 16:21:22 -0400

    done 16/4

--- Chp16/src/chp16.scala
+++ Chp16/src/chp16.scala
@@ -4,11 +4,11 @@
 object chp16 {
 
   def main(args: Array[String]) {
-    def printAll(numbers: Int*) {
-      numbers.foreach(println)
-    }
+    //def printAll(numbers: Int*) {
+    //  numbers.foreach(println)
+    //}
 
-    printAll(1,2,3,4)
+    //printAll(1,2,3,4)
 
     val fruit = List("apples","oranges","pears")
     val nums = List(1,2,3,4)
@@ -28,6 +28,22 @@ object chp16 {
     val empty1 = Nil
 
     diag3.foreach(println)
+
+    println(empty.isEmpty)
+    println(fruit.isEmpty)
+    println(fruit.head)
+    println(fruit.tail.head)
+    println(diag3.head)
+
+    def isort(xs: List[Int]): List[Int] = {
+      if (xs.isEmpty) Nil
+      else insert(xs.head, isort(xs.tail))
+    }
+
+    def insert(x: Int, xs: List[Int]): List[Int] = {
+      if (xs.isEmpty || x <= xs.head) x :: xs
+      else xs.head :: insert(x, xs.tail)
+    }
 
 
 
