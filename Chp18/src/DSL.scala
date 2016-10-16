0773841f1feb0eaa8cc14345cd9597a9cb3aee94
Freshield <zxdsw199182@gmail.com>
2016-10-16 18:52:51 -0400

    done 18.6

--- Chp18/src/DSL.scala
+++ Chp18/src/DSL.scala
@@ -1,37 +1,29 @@
 /**
   * Created by FRESHIELD on 2016/10/15.
   */
-object DSL {
+object DSL extends CircuitSimulation{
+
+  def InverterDelay = 1
+  def AndGateDelay = 3
+  def OrGateDelay = 5
 
   def main(args: Array[String]) {
 
+    val input1, input2, sum, carry = new Wire
+    probe("sum", sum)
+    probe("carry", carry)
+
+    halfAdder(input1, input2, sum, carry)
+
+    input1 setSignal true
+
+    run()
+
+    input2 setSignal true
+
+    run()
+
   }
-
-  def inverter(input: Wire, output: Wire): Unit ={
-
-  }
-  def andGate(a1: Wire, a2: Wire, output: Wire): Unit ={
-
-  }
-  def orGate(o1: Wire, o2: Wire, output: Wire): Unit ={
-
-  }
-
-  def halfAdder(a: Wire, b: Wire, s: Wire, c: Wire): Unit = {
-    val d, e = new Wire
-    orGate(a, b, d)
-    andGate(a, b, c)
-    inverter(c, e)
-    andGate(d, e, s)
-  }
-
-  def fullAdder(a: Wire, b: Wire, cin: Wire, sum: Wire, cout: Wire): Unit ={
-    val s, c1, c2 = new Wire
-    halfAdder(a, cin, s, c1)
-    halfAdder(b, s, sum, c2)
-    orGate(c1, c2, cout)
-  }
-
 
 
 }
