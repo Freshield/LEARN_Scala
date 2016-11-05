1a480039c04bbf39440e8ac27df5608859cf239e
Freshield <zxdsw199182@gmail.com>
2016-11-05 15:54:40 -0400

    done 22.1

--- Chp22/src/chp22.scala
+++ Chp22/src/chp22.scala
@@ -4,7 +4,18 @@
 object chp22 {
 
   def main(args: Array[String]) {
+    val xs = List(1,2,3)
+    val ys: List[Any] = xs
+    ys.foreach(println)
 
+    abstract class Fruit
+    class Apple extends Fruit
+    class Orange extends Fruit
+
+    val apples = new Apple :: Nil
+    println(apples)
+    val fruits = new Orange :: apples
+    println(fruits)
   }
 
 }
