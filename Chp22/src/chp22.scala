daed9bdac1b66311c3d29b5cb20fc638b0eeb28c
Freshield <zxdsw199182@gmail.com>
2016-11-05 16:57:21 -0400

    done 22.2

--- Chp22/src/chp22.scala
+++ Chp22/src/chp22.scala
@@ -1,6 +1,8 @@
 /**
   * Created by FRESHIELD on 2016/11/5.
   */
+import scala.collection.mutable.ListBuffer
+
 object chp22 {
 
   def main(args: Array[String]) {
@@ -16,6 +18,17 @@ object chp22 {
     println(apples)
     val fruits = new Orange :: apples
     println(fruits)
+
+    def incAll(xs: List[Int]): List[Int] = {
+      val buf = new ListBuffer[Int]
+      for (x <- xs) buf += x + 1
+      buf.toList
+    }
+
+    val intList = List(1,2,3,4)
+    val intList1 = incAll(intList)
+    intList1.foreach(println)
+
   }
 
 }
