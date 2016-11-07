03d7c926266a4bf40a374a332859e728ff71fee4
Freshield <zxdsw199182@gmail.com>
2016-11-06 20:23:59 -0500

    done 23.0

--- Chp23/src/chp23.scala
+++ Chp23/src/chp23.scala
@@ -4,7 +4,22 @@
 object chp23 {
 
   def main(args: Array[String]) {
-    
+    val lara = Person("Lara", false)
+    val bob = Person("Bob", true)
+    val julie = Person("Julie", false, lara, bob)
+    val persons = List(lara, bob, julie)
+    val pairs = persons filter (p => !p.isMale) flatMap (p => (p.children map (c => (p.name, c.name))))
+    pairs.foreach(println)
+
+    val pair1 = for (p <- persons; if !p.isMale; c <- p.children)
+      yield (p.name, c.name)
+
+    pair1.foreach(println)
+
   }
+
+  case class Person(name: String,
+                    isMale: Boolean,
+                    children: Person*)
 
 }
