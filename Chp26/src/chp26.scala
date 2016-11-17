2200980d69ed11098b1582138f37e5cfbcff897f
Freshield <zxdsw199182@gmail.com>
2016-11-17 11:04:09 -0500

    import xml jar

--- Chp26/src/chp26.scala
+++ Chp26/src/chp26.scala
@@ -1,10 +1,17 @@
 /**
   * Created by FRESHIELD on 2016/11/17.
   */
+import scala.xml._
+
 object chp26 {
 
   def main(args: Array[String]) {
-    
+    val elem = <a>
+    This is some XML.
+    Here is a tag: <atag/>
+    </a>
+
+    println(elem)
   }
 
 }
