f8dcce1dacca1fe64260bef2c1e656f2575ee568
Freshield <zxdsw199182@gmail.com>
2016-11-23 14:35:48 -0500

    done basic

--- Test_Json/src/Driver.scala
+++ Test_Json/src/Driver.scala
@@ -8,11 +8,7 @@ import net.liftweb.json.JsonDSL._
 object Driver {
 
   def main(args: Array[String]) {
-    val jsonString =
-      """ {"user":{
-        |"user1":{"name":"Unmi","email":"fantasia@sina.com"},
-        |"user2":{"name":"Unmi.cc","email":"unmi@unmi.cc"}
-        |}}""".stripMargin
+    val jsonString =parse("""{"numbers":"[1,2,3,4]"}""")
 
     val json =
       ("person" ->
@@ -25,11 +21,28 @@ object Driver {
               )
             )
         )
-    println(compact(render(json \ "person" \ "name")))
-    println(json)
-    //println(maybeName)
-    //val emails = (json \ "user" \\ "email").map(_.as[String])
-    //println(emails)
+    println(json \ "person" \ "name")
+    println(jsonString \ "numbers")
+
+    val json1 = List(1,2,3)
+    println(json1)
+    println(compact(render(json1)))
+
+    val json2 = {"name" -> "joe"}
+    println(json2)
+    println(compact(render(json2)))
+
+    val json3 = ("name" -> "joe") ~ ("age" -> 35)
+    println(json3)
+    println(compact(render(json3)))
+
+    val json4 = ("name" -> "joe") ~ ("age" -> Some(35))
+    println(json4)
+    println(compact(render(json4)))
+
+    val json5 = ("name" -> "joe") ~ ("age" -> (None: Option[Int]))
+    println(json5)
+    println(compact(render(json5)))
   }
 
 }
